9ef8e6fe3f53d51a3c1dcc64183fec326149bcde
Ethan Lijin <ethanlijincs@gmail.com>
2024-09-27 10:58:00 -0400

    added for loop to returnCopy idk how it deleted last time; added arrayToString code

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -1,11 +1,23 @@
 public class myArrays.java {
-  public static int[] returnCopy (int[] arr){
-    int[] thing = new int[arr.length]
-      thing[i] = arr[i];
+  public static int[] returnCopy (int[] ary){
+    int[] thing = new int[ary.length]
+    for (int i = 0; i < ary.length; i++){
+      thing[i] = ary[i];
     }
     return thing;
+  }
+  public static int concatArray(int[] ary1, int[] ary2){
+    int[] arr = new int[ary1.length + ary2.length];
+    for )
   }
   public static void main(String[] args){
 
   }
+  public static arrayToString(int[] nums){
+    String[] ary = new String[nums.length]
+    for (int i = 0; i < nums.length; i++){
+      ary[i] = "" + nums[i];
+    }
+  }
+  return ary;
 }
