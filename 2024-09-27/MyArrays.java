630f80c09174253956c9c94e9da829e4e2d999aa
Ethan Lijin <ethanlijincs@gmail.com>
2024-09-27 10:50:58 -0400

    added code for returnCopy

---
+++ 2024-09-27/MyArrays.java
@@ -0,0 +1,11 @@
+public class myArrays.java {
+  public static int[] returnCopy (int[] arr){
+    int[] thing = new int[arr.length]
+      thing[i] = arr[i];
+    }
+    return thing;
+  }
+  public static void main(String[] args){
+
+  }
+}
