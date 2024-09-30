41bf880222d4b0a1582a5022f1f4e2f74106b5e9
EthanLijin <ethanlijincs@gmail.com>
2024-09-29 22:03:56 -0400

    added testcases, and tweaked code for concat array

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -1,29 +1,46 @@
-public class myArrays.java {
+public class MyArrays {
   public static int[] returnCopy (int[] ary){
-    int[] thing = new int[ary.length]
+    int[] thing = new int[ary.length];
     for (int i = 0; i < ary.length; i++){
       thing[i] = ary[i];
     }
     return thing;
   }
-  public static int concatArray(int[] ary1, int[] ary2){
+  public static int[] concatArray(int[] ary1, int[] ary2){
     int sumLength = ary1.length + ary2.length;
-    int[] arr = new int[length];
+    int[] arr = new int[sumLength];
     for (int i = 0; i < ary1.length; i++){
       arr[i] = ary1[i];
     }
-    for (int i = ary1.length; i < ary2.length; i++){
-      arr[i] = ary2[i];
+    for (int i = 0; i < ary2.length; i++){
+      arr[ary1.length + i] = ary2[i];
     }
     return arr;
   }
-  public static void main(String[] args){
-  }
-  public static arrayToString(int[] nums){
-    String[] ary = new String[nums.length]
+
+  public static String[] arrayToString(int[] nums){
+    String[] ary = new String[nums.length]; 
     for (int i = 0; i < nums.length; i++){
       ary[i] = "" + nums[i];
     }
+    return ary;
   }
-  return ary;
+  public static void main(String[] args){
+    int[] original = {0,1,2,3,4};
+    int[] copy = returnCopy(original);
+
+    System.out.println("Original and returnCopy Test" + java.util.Arrays.equals(original,copy));
+
+    int[] part1 = {1,2};
+    int[] part2 = {3,4};
+
+    System.out.println("Concated Array is " + java.util.Arrays.toString(concatArray(part1, part2)));
+
+    int[] stringArray = {1, 2, 3, 4};
+    
+    System.out.println("int array to strings " + java.util.Arrays.toString(arrayToString(stringArray)));
+  }
 }
+  
+
+
