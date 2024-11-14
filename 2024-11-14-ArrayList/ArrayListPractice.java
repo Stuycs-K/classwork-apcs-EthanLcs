4085d44a6e96aa9c989976551952ceac295d42de
EthanLijin <ethanlijincs@gmail.com>
2024-11-14 10:38:07 -0500

    did replaceEmpty

--- 2024-11-14-ArrayList/ArrayListPractice.java
+++ 2024-11-14-ArrayList/ArrayListPractice.java
@@ -15,8 +15,12 @@ public class ArrayListPractice {
    public static void replaceEmpty( ArrayList<String> original){
   //Modify the ArrayList such that it has all of the empty strings are
   //replaced with the word "Empty".
-
-    
+    for (int i = 0; i < original.size(); i++){
+        if (original.get(i).equals("")){
+            original.set(i, "Empty");
+        }
+    }
+    return original;
     } 
     public static ArrayList<String> makeReversedList( ArrayList<String> original){
   //return a new ArrayList that is in the reversed order of the original.
