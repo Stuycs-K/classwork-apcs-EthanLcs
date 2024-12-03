c4f804be3e360c6b65c406d6c852959d2c4bd6f6
Ethan Lijin <ethanlijincs@gmail.com>
2024-12-03 11:09:22 -0500

    started

---
+++ 2024-12-03-colors/ColorDemo.java
@@ -0,0 +1,8 @@
+public class ColorDemo{
+  public static final String Red = "\u001b[31m";
+
+  public static void main(String[] args){
+    System.out.print(Red);
+    System.out.print("this is red");
+  }
+}
