032a29b2b29fddde91d612f7ac2727a5a93185b4
EthanLcs <ethanlijincs@gmail.com>
2024-12-05 08:38:37 -0500

    kida did draw

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -14,16 +14,16 @@ public class Driver {
         Text.color(Text.WHITE, Text.background(Text.BLUE));
         for (int col = 0; col <= 80; col++){
             Text.go(1,col);
-            System.out.println(" ");
+            System.out.print(" ");
             Text.go(30,col);
-            System.out.println(" ");
+            System.out.print(" ");
         }
 
         for (int row = 0; row <= 30; row++){
             Text.go(row,1);
-            System.out.println(" ");
+            System.out.print(" ");
             Text.go(row, 80);
-            System.out.println(" ");
+            System.out.print(" ");
         }
     }
 
@@ -38,11 +38,19 @@ public class Driver {
     public static void drawSeperator(){
         Text.go(3,1);
         for (int col = 0; col <= 80; col++){
-            System.out.println("-");
+            System.out.print("-");
         }
     }
 
-    public static void draw(){
-        int spacing = 80/ 
+    public static void draw(int[] numbers){
+        int spacing = 80 / numbers.length + 1;
+        for (int i = 0; i < numbers.length; i++){
+            Text.go(2, spacing *(i + 1));
+            if (numbers[i] < 25) Text.color(Text.RED, Text.BRIGHT);
+            else if (numbers[i] > 75) Text.color(Text.GREEN, Text.BRIGHT);
+            else {
+                Text.color(Text.WHITE);
+            }
+        }
     }
 }
