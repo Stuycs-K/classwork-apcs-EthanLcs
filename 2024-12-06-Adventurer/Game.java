f59c36fa45186796190a63b32577268345ae631a
Ethan Lijin <ethanlijincs@gmail.com>
2024-12-09 10:58:41 -0500

    did part of the user input stuff

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -3,15 +3,29 @@ public class Game{
   Adventurer p2 = new Codewarrior("bobby", 50, "python");
 
   public static void main(String[] args){
+    System.out.println(p1.getName() + p1.getHP());
+    System.out.println(p2.getName() + p2.getHP());
+
     Scanner userInput = new Scanner(System.in);
 
     //You can do the rest many times:
 
-    System.out.println("Enter username");
+    System.out.println("Type: (a)ttack / (sp)ecial / (su)pport" / quit");
     //Read one line of user input
     String userName = userInput.nextLine();
+    if (input.equals("attack")){
+      p1.attack(p2);
+    }
+    else if (input.equals("special"){
+      p1.specialAttack(p2);
+    }
+    else if (input.equals("support"){
+      p1.support(p2);
+    }
+    else {
+      System.exit();
+    }
     //Do something with the input
-    System.out.println("Username is: " + userName);
 
   }
 
