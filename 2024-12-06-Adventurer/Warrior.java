0720da9189c7045a4476d5e49f75f91758963ba1
EthanLcs <ethanlijincs@gmail.com>
2024-12-09 08:03:36 -0500

    did constructors

---
+++ 2024-12-06-Adventurer/Warrior.java
@@ -0,0 +1,38 @@
+public class Warrior extends Adventurer {
+    
+    public Warrior(String name){
+        super(name);
+    }
+
+    public Warrior(String name, int hp){
+        super(name, hp);
+    }
+
+    String getSpecialName(){
+        
+    }
+
+    int getSpecial(){
+
+    }
+    void getSpecial(){
+
+    }
+    int getSpecialMax(){
+
+    }
+
+    String attack(Adventurer other){
+
+    }
+    
+    String support(Adventurer other){
+
+    }
+    String support(){
+
+    }
+    String specialAttack(Adventurer other){
+            
+    }
+}
